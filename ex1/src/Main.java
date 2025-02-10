d0fceea5039bfbdde74eec84fed552020d9144a7
sofia groke <137205924+aguainsalubre@users.noreply.github.com>
2025-02-10 18:39:32 -0300

    Add files via upload

---
+++ ex1/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args){
+
+        int lanche1 = 10;
+        int lanche2 = 4;
+        int lanche3 = 2;
+
+            int total = lanche1 + lanche2 + lanche3;
+
+            int media = total/3;
+            float media2 = (float) media;
+
+        System.out.println("Total de lanches " + total);
+        System.out.println("Média dos lanches " + media2);
+
+    }
+}
